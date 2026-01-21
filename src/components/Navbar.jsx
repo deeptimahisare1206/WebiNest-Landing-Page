80fafd349d3c447d7778a3f14550a3fffea727dd
deeptimahisare1206 <deeptimahisare@gmail.com>
2026-01-21 22:03:23 +0530

    navbar responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-white shadow-md">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-4 lg:px-8">
         <div className="flex justify-between h-20 items-center">
           <div className="">
             <img src={logo} alt="Logo" className="h-20 w-auto" />
